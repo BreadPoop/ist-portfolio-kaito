adaa4682842b28143ca6d2907e988b7a91b4cf7a
BreadPoop <2027relova.kaito@fcboe.org>
2024-01-22 14:47:58 -0500

    Created basics file

---
+++ c/basics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This is my C basics file.\n");
+	
+	int num;
+	printf("What is your number? ");
+	scanf("%i", &num);
+	printf("Your number is %i\n", num);
+	
+	return 0;
+}
